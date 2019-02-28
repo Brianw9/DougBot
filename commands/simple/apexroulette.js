6f8d37c6b1c141b8ea36586daecb785ed5ee8984
Brianw9 <32607411+Brianw9@users.noreply.github.com>
2019-02-27 21:11:38 -0500

    Update apexroulette.js

--- commands/simple/apexroulette.js
+++ commands/simple/apexroulette.js
@@ -13,80 +13,13 @@ class apexRouletteCommand extends commando.Command
     }
     async run(message, args)
     {
-        var diceRoll = Math.floor(Math.random() * 18) + 1;
-        if (diceRoll == 1)
-        {
-            message.channel.send("Go Slum Lakes");
-        }
-        if (diceRoll == 2)
-        {
-            message.channel.send("Drop Runoff");
-        }
-        if (diceRoll == 3)
-        {
-            message.channel.send("Drop at The Pit");
-        }
-        if (diceRoll == 4)
-        {
-            message.channel.send("Drop Artillery");
-        }
-        if (diceRoll == 5)
-        {
-            message.channel.send("Drop Cascades")
-        }
-        if (diceRoll == 6)
-        {
-            message.channel.send("Drop Relay")
-        }
-        if (diceRoll == 7)
-        {
-            message.channel.send("Drop Wetlands")
-        }
-        if (diceRoll == 8)
-        {
-            message.channel.send("Drop Bunker")
-        }
-        if (diceRoll == 9)
-        {
-            message.channel.send("Drop Airbase")
-        }
-        if (diceRoll == 10)
-        {
-            message.channel.send("Drop Bridges")
-        }
-        if (diceRoll == 11)
-        {
-            message.channel.send("Drop Skull Town")
-        }
-        if (diceRoll == 12)
-        {
-            message.channel.send("Drop Thunderdome")
-        }
-        if (diceRoll == 13)
-        {
-            message.channel.send("Drop Water Treatment")
-        }
-        if (diceRoll == 14)
-        {
-            message.channel.send("Drop Market")
-        }
-        if (diceRoll == 15)
-        {
-            message.channel.send("Drop Hydro Dam")
-        }
-        if (diceRoll == 16)
-        {
-            message.channel.send("Drop Repulsor")
-        }
-        if (diceRoll == 17)
-        {
-            message.channel.send("Drop Swamps, easy win, easy life.")
-        }
-        if (diceRoll == 18)
-        {
-            message.channel.send("HOTZONE BOIIIIII")
-        }
-
+        var dropAreaRoll = Math.floor(Math.random() * 18) + 1;
+        var prefixRoll = Math.floor(Math.random() * 3) + 1;
+        var dropAreas = ["Slum Lakes", "Runoff", "The Pit", "Artillery", "Cascades", "Relay", "Wetlands", "Bunker", "Airbase", "Bridges",
+         "Skull Town", "Airbase", "Bridges", "Skull Town", "Thunderdome", "Water Treatment", "Market", "Hydro Dam", "Repulsor", "Swamps", "HOTZONE BOIIIIII"];
+        var prefixes = ["Go ", "Buckle up, we're goin\' ", "Really? Alright just go to "]
+        
+        message.channel.send(prefixes[prefixRoll] + dropAreas[dropAreaRoll]);
 
     }
 }
