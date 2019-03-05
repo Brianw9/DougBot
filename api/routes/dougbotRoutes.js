8156b541138794950c824b5b04aa5424180a30a8
Brianw9 <32607411+Brianw9@users.noreply.github.com>
2019-03-05 04:05:46 -0500

    MongoDB added

---
+++ api/routes/dougbotRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+    var dougbot = require('../controllers/dougbotController');
+
+    //dougbot Routes
+    app.route('/tasks')
+    .get(dougbot.list_all_tasks)
+    .post(dougbot.create_a_task);
+
+
+  app.route('/tasks/:taskId')
+    .get(dougbot.read_a_task)
+    .put(dougbot.update_a_task)
+    .delete(dougbot.delete_a_task);
+};
