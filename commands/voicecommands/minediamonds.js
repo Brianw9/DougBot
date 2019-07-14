842470c427e3a467b54a08b4df440e420f42a018
Brianw9 <32607411+Brianw9@users.noreply.github.com>
2019-07-14 17:14:19 -0400

    Create minediamonds.js

---
+++ commands/voicecommands/minediamonds.js
@@ -0,0 +1,38 @@
+const commando = require('discord.js-commando');
+const ytdl = require('ytdl-core');
+const opus = require('opusscript');
+
+class MineDiamondsCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'minediamonds',
+            group: 'voice commands',
+            memberName: 'minediamonds',
+            description: 'Take me away',
+            throttling: {
+                usages: 2,
+                duration: 30,
+            }
+        })
+    }
+    async run(message, args) {
+        var VC = message.member.voiceChannel;
+        if (message.member.voiceChannel) {
+            VC.join()
+                .then(connection => {
+                    const dispatcher = connection.playStream(ytdl(
+                        'https://www.youtube.com/watch?v=dgha9S39Y6M', {
+                            filter: 'audioonly'
+                        }));
+                    dispatcher.on("end", end => {
+                        VC.leave()
+                    });
+                })
+                .catch(console.error);
+        } else {
+            message.reply("You aren't in a voice channel.")
+        }
+    }
+}
+
+module.exports = MineDiamondsCommand;
